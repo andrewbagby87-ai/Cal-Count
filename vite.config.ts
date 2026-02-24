b4785321abd8d2254d82ab9a03b9dd707f6e6618
andrewbagby87-ai <andrewbagby87@gmail.com>
2026-02-24 18:46:09 -0500

    GitHub pages fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Cal-Count/', // Add this line exactly as shown
   plugins: [
     react(),
     VitePWA({
